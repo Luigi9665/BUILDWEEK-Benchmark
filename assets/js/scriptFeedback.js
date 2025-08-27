bdac8c88c5c53b9d6613a8de1bf95825eab4499c
Dobri Dimov Dobrev <126101328+dobridimovdobrev@users.noreply.github.com>
2025-08-27 17:07:48 +0200

    adding script in the feedback page

---
+++ assets/js/scriptFeedback.js
@@ -0,0 +1,49 @@
+/* Variables */
+const rating = document.querySelector('.rating');
+
+const comment = document.getElementById('comment');
+const submitBtn = document.getElementById('submit');
+const commentsList = document.getElementById('comments-list');
+
+const feedbackBtn = document.querySelector('.btn-general');
+const starColor = document.querySelectorAll('.star');
+feedbackBtn.classList.add('no-before');
+
+
+/* Rating system */
+rating.addEventListener('click', (event) => {
+  if (event.target.classList.contains('star')) {
+
+    const starValue = event.target.getAttribute('data-value');
+     starColor.forEach(star => {
+      if (parseInt(star.getAttribute('data-value')) <= parseInt(starValue)) {
+        star.classList.add('star-color');
+      } else {
+        star.classList.remove('star-color');
+      }
+    });
+  }
+});
+
+/* Input */
+comment.addEventListener('input', () => {
+  if(comment.value.trim() === ''){
+    feedbackBtn.classList.add('no-before');
+  } else {
+    feedbackBtn.classList.remove('no-before');
+  }
+});
+
+/* Submit Button */
+submitBtn.addEventListener('click', () => {
+
+  if(comment.value.trim() !== ""){
+    
+    const newListItem = document.createElement('li');
+    commentsList.appendChild(newListItem);
+    newListItem.innerHTML = comment.value;
+    comment.value = "";
+    feedbackBtn.classList.add('no-before'); 
+  }
+});
+
