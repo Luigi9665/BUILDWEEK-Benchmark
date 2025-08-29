d8665697a4553f864941f9d2f482589f377ad96c
Luigi9665 <ventriglia.luigi@hotmail.com>
2025-08-29 16:29:04 +0200

    completato

--- assets/js/thanksgiving.js
+++ assets/js/thanksgiving.js
@@ -11,24 +11,31 @@ const secondDesc = document.querySelector("#secondDescription");
 const ul = document.querySelector("#comments-list");
 const comment = localStorage.getItem("comment");
 const starValue = localStorage.getItem("starValue");
-console.log("commento:", comment);
-console.log("valutazione:", starValue);
 const starColor = document.querySelectorAll(".star");
+const li = document.createElement("li");
+
+// padding: 2rem;
+//     width: 70%;
+
+const divfeedback = document.querySelector(".feedback-box");
+divfeedback.style.padding = "2rem";
+divfeedback.style.width = "65%";
 
 // illumino le stelle
-const lumos = (value) => {
+const lumos = (value, comment) => {
   starColor.forEach((star) => {
     if (parseInt(star.dataset.value) <= value) {
       star.classList.add("star-color");
     } else {
       star.classList.remove("star-color");
     }
+    li.innerText = comment;
+    ul.appendChild(li);
   });
 };
 
 // i valori passati dalla pagina JS del feedback
 
-const generationTitle = (starValue) => {};
 const generationFirstDescription = (value) => {
   if (value <= 4) {
     firstDesc.innerText = "We're sorry for your experience. We'll work to improve.";
@@ -42,6 +49,6 @@ const generationFirstDescription = (value) => {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-  lumos(starValue);
+  lumos(starValue, comment);
   generationFirstDescription(starValue);
 });
